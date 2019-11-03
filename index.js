4c8ddc191bca631a166bd312d8cb8493f598c8c8
FridaPolished <fridapulido@gmail.com>
2019-11-03 14:50:41 -0800

    Complete graphql setup

--- index.js
+++ index.js
@@ -1,6 +1,85 @@
 const express = require("express");
 const app = express();
+const db = require('./config/keys').mongoURI;
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const expressGraphQL = require('express-graphql');
+const User = require("./models/user");
+const Post = require("./models/post");
+const schema = require("./schema/schema");
+// app.get("/", (req, res) => res.send("fun with cats"));
+app.use(bodyParser.json());
 
 
-app.get("/", (req, res) => res.send("Hello World this is more fun with cats"));
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: true
+//   })
+// );
+
+// const router = express.Router();
+
+// const createNewUser = router.post("/new", (req, res) => {
+//   User.findOne({ email: req.body.email }).then(user => {
+//     if (user) {
+//       // Throw a 400 error if the email address already exists
+//       return res
+//         .status(400)
+//         .json({ email: "A user has already registered with this address" });
+//     } else {
+//       // Otherwise create a new user
+//       console.log(req.body);
+//       const newUserObj = new User({
+//         name: req.body.name,
+//         email: req.body.email,
+//         password: req.body.password
+//       });
+
+//       newUserObj
+//         .save()
+//         .then(savedUser => res.json(savedUser))
+//         .catch(err => console.log(err));
+//     }
+//   });
+// });
+
+// app.use("/users", createNewUser);
+
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: true
+//   })
+// );
+
+// const router = express.Router();
+
+// const createNewPost = router.post("/new", (req, res) => {
+//   // remember to import your Post model from Mongoose!
+//   const newPost = new Post({
+//     title: req.body.title,
+//     body: req.body.body,
+//     date: req.body.date,
+//     author: req.body.author
+//   });
+
+//   newPost
+//     .save()
+//     .then(savedPost => res.json(savedPost))
+//     .catch(err => console.log(err));
+// });
+
+// app.use("/posts", createNewPost);
+
 app.listen(5000, () => console.log('Server is running on port 5000'));
+app.use(
+  "/graphql",
+  expressGraphQL({
+    schema,
+    graphiql: true
+  })
+);
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
